8d747ea9d836b0560a1eb4042211af695f6852df
Yan Soares <yts.1995@gmail.com>
2022-03-14 10:00:29 -0300

    chore: initial server configs done

---
+++ server/config.js
@@ -0,0 +1,28 @@
+import { join, dirname } from 'path'
+import { fileURLToPath } from 'url'
+
+const currentDir = dirname(fileURLToPath(import.meta.url))
+const root = join(currentDir, '../')
+
+const audioDir = join(root, 'audio')
+const publicDir = join(root, 'public')
+const songsDir = join(audioDir, 'songs')
+const fxDir = join(audioDir, 'fx')
+
+export default {
+  port: process.env.PORT || 3000,
+  dir: {
+    root,
+    audioDir,
+    publicDir,
+    songsDir,
+    fxDir
+  },
+  pages: {
+    homeHTML: 'home/index.html',
+    controllerHTML: 'controller/index.html'
+  },
+  location: {
+    home: '/home'
+  }
+}
